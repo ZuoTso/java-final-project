04feb24f40321272b6840c930e8cb6f9fc3317c1
ascodeasice <ab0988956087@gmail.com>
2023-04-20 16:41:23 +0800

    Add RestaurantViewModel

---
+++ app/src/main/java/com/example/nckujavafinalproject/RestaurantViewModel.java
@@ -0,0 +1,29 @@
+package com.example.nckujavafinalproject;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class RestaurantViewModel extends AndroidViewModel {
+
+    private RestaurantRepository mRepository;
+
+    private final LiveData<List<Restaurant>> mAllRestaurants;
+
+    public RestaurantViewModel(Application application) {
+        super(application);
+        mRepository = new RestaurantRepository(application);
+        mAllRestaurants = mRepository.getAllRestaurants();
+    }
+
+    LiveData<List<Restaurant>> getAllRestaurants() {
+        return mAllRestaurants;
+    }
+
+    public void insert(Restaurant restaurant) {
+        mRepository.insert(restaurant);
+    }
+}
