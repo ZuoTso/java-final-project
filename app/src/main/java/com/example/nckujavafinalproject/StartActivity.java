9be24704bcf72a5534d82cbb12b729d10e508465
ascodeasice <ab0988956087@gmail.com>
2023-04-30 15:07:31 +0800

    Click edit restaurant button go to restaurant list page

--- app/src/main/java/com/example/nckujavafinalproject/StartActivity.java
+++ app/src/main/java/com/example/nckujavafinalproject/StartActivity.java
@@ -2,7 +2,12 @@ package com.example.nckujavafinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    // to restaurant page button
+    public void EditRestaurant_onclick(View view){
+        // switch to restaurant page
+        Intent intent = new Intent();
+        intent.setClass(StartActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
