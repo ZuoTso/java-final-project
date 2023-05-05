4f3742ea26904e3178568cda27527f1a1e050d6d
ascodeasice <ab0988956087@gmail.com>
2023-05-05 14:18:27 +0800

    Create UpdateRestaurantActivity

---
+++ app/src/main/java/com/example/nckujavafinalproject/UpdateRestaurantActivity.java
@@ -0,0 +1,14 @@
+package com.example.nckujavafinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateRestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_restaurant);
+    }
+}
