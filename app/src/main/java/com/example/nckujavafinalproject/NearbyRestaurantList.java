d1c6bf7393041dbf6565f261bdae9ae9808dfa72
ascodeasice <ab0988956087@gmail.com>
2023-05-15 13:42:28 +0800

    Create button to switch to nearby restaurants list

---
+++ app/src/main/java/com/example/nckujavafinalproject/NearbyRestaurantList.java
@@ -0,0 +1,14 @@
+package com.example.nckujavafinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NearbyRestaurantList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nearby_restaurant_list);
+    }
+}
