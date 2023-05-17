0d84667fa1679a23215ab49101b9625006c651cd
ascodeasice <ab0988956087@gmail.com>
2023-05-17 11:21:18 +0800

    Add function to calculate distance from coords

--- app/src/main/java/com/example/nckujavafinalproject/NearbyRestaurantList.java
+++ app/src/main/java/com/example/nckujavafinalproject/NearbyRestaurantList.java
@@ -58,7 +58,7 @@ public class NearbyRestaurantList extends AppCompatActivity {
         String apiKey = BuildConfig.MAPS_API_KEY;
 
         // SECTION test fetching
-        final int radius=1500;
+        final int radius = 1500;
 
         String url = String.format(
                 "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%f,%f&radius=%d&type=restaurant&key=%s",
@@ -83,7 +83,7 @@ public class NearbyRestaurantList extends AppCompatActivity {
             public void onResponse(Call call, Response response) throws IOException {
                 // Handle the response on the background thread
                 String responseBody = response.body().string();
-                Log.v("INFO",responseBody);
+                Log.v("INFO", responseBody);
 
                 try {
                     JSONObject json = new JSONObject(responseBody);
@@ -99,5 +99,30 @@ public class NearbyRestaurantList extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    // return meters between two locations
+    private double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+        // Earth's radius in kilometers
+        double radius = 6371;
+
+        // Convert latitude and longitude from degrees to radians
+        double lat1Rad = Math.toRadians(lat1);
+        double lon1Rad = Math.toRadians(lon1);
+        double lat2Rad = Math.toRadians(lat2);
+        double lon2Rad = Math.toRadians(lon2);
+
+        // Calculate the differences between the latitude and longitude values
+        double latDiff = lat2Rad - lat1Rad;
+        double lonDiff = lon2Rad - lon1Rad;
+
+        // Calculate the distance using the Haversine formula
+        double a = Math.sin(latDiff / 2) * Math.sin(latDiff / 2) +
+                Math.cos(lat1Rad) * Math.cos(lat2Rad) *
+                        Math.sin(lonDiff / 2) * Math.sin(lonDiff / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = radius * c * 1000;
+
+        return distance;
     }
 }
