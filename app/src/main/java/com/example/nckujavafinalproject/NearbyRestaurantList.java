ea54664bb97271ee048a63780ff9190bf2130079
ascodeasice <ab0988956087@gmail.com>
2023-05-17 11:20:39 +0800

    Fetch maps api when nearby activity is created

--- app/src/main/java/com/example/nckujavafinalproject/NearbyRestaurantList.java
+++ app/src/main/java/com/example/nckujavafinalproject/NearbyRestaurantList.java
@@ -9,10 +9,22 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class NearbyRestaurantList extends AppCompatActivity {
 
@@ -37,6 +49,55 @@ public class NearbyRestaurantList extends AppCompatActivity {
 
         currentLat = getIntent().getDoubleExtra("lat", 0.0);
         currentLng = getIntent().getDoubleExtra("lng", 0.0);
-        // TODO: request data with current location
+        // request data with current location
+        fetchData();
+    }
+
+    private void fetchData() {
+        // SECTION get api key from local.properties
+        String apiKey = BuildConfig.MAPS_API_KEY;
+
+        // SECTION test fetching
+        final int radius=1500;
+
+        String url = String.format(
+                "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%f,%f&radius=%d&type=restaurant&key=%s",
+                currentLat,
+                currentLng,
+                radius,
+                apiKey);
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                // Handle the response on the background thread
+                String responseBody = response.body().string();
+                Log.v("INFO",responseBody);
+
+                try {
+                    JSONObject json = new JSONObject(responseBody);
+                    JSONArray results = json.getJSONArray("results");
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+                // Update the UI if needed (switch to the main thread)
+                runOnUiThread(() -> {
+                    // Update UI here
+
+                });
+            }
+        });
     }
 }
