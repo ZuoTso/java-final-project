29b5d9536900826222205548fbd0946a239a34ca
ascodeasice <ab0988956087@gmail.com>
2023-05-31 09:43:11 +0800

    Remove dark mode

--- app/src/main/java/com/example/nckujavafinalproject/MainActivity.java
+++ app/src/main/java/com/example/nckujavafinalproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.nckujavafinalproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -43,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // remove dark mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
         String savedLabels=sharedPref.getString("selectedLabels","");
